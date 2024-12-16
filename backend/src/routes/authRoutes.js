e8592ebe1be55248bcb2f56bd47ab75abb0dfc1a
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2024-12-16 23:06:50 +0530

    added user Sechma

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const router = express.router()
+const router = express.Router()
+const authController = require("../controllers/authController")
+const authMiddleware = require("../middleware/authMiddleware")
 
-router.post("/signup",(req,res)=>{
- res.send("signup route")
-})
-router.post("/login",(req,res)=>{
- res.send("login route")
-})
-router.post("/logout",(req,res)=>{
- res.send("logout route")
-})
+router.post("/signup",authController.signup)
+router.post("/login",authController.login)
+router.post("/logout",authController.logout)
+router.post("/profile",authMiddleware,authController.getProfile)
+module.exports =router()
+ 
+ 
