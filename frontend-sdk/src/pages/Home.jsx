085e071ece48b3a95f44a956f947c033f8257a0e
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-01-02 14:48:22 +0530

    added Link so that it wont create  any issue

--- frontend-sdk/src/pages/Home.jsx
+++ frontend-sdk/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate,Link } from "react-router-dom";
 function Home() {
   const isAuthenticated = !!localStorage.getItem("token");
 
